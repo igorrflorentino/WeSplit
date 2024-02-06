c8763ac52832ac4d215263886ea20dbbe549c748
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-02-06 17:13:13 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Igor Florentino on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
